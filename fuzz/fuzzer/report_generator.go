b4ba8c281b035fc95904e883cb1c231a46363111
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2024-11-26 18:39:16 -0800

    Add pipeline job to merge reports

--- fuzz/fuzzer/report_generator.go
+++ fuzz/fuzzer/report_generator.go
@@ -3,43 +3,44 @@ package fuzzer
 import (
 	"fmt"
 	"io"
-	"log"
 	"os"
 )
 
-func main() {
+// GenerateReport consolidates multiple fuzzing reports into a final one.
+func GenerateReport() error {
 	// Open the first report (fuzzing report)
-	fuzzingReport, err := os.Open("./fuzzer/reports/report_fuzzer.txt")
+	fuzzingReport, err := os.Open("./fuzzing-report/report.txt")
 	if err != nil {
-		log.Fatalf("Failed to open fuzzing report: %v", err)
+		return fmt.Errorf("failed to open fuzzing report: %v", err)
 	}
 	defer fuzzingReport.Close()
 
 	// Open the second report (fuzzing_with_seeds report)
-	fuzzingWithSeedsReport, err := os.Open("./fuzzer/reports/report_custom_inputs.txt")
+	fuzzingWithSeedsReport, err := os.Open("./fuzzing-with-seeds-report/report.txt")
 	if err != nil {
-		log.Fatalf("Failed to open fuzzing_with_seeds report: %v", err)
+		return fmt.Errorf("failed to open fuzzing_with_seeds report: %v", err)
 	}
 	defer fuzzingWithSeedsReport.Close()
 
-	// Create the consolidated report file
+	// Create the consolidated final report file
 	finalReport, err := os.Create("final_report.txt")
 	if err != nil {
-		log.Fatalf("Failed to create final report file: %v", err)
+		return fmt.Errorf("failed to create final report file: %v", err)
 	}
 	defer finalReport.Close()
 
 	// Write the fuzzing report to the final report
 	_, err = io.Copy(finalReport, fuzzingReport)
 	if err != nil {
-		log.Fatalf("Failed to copy fuzzing report: %v", err)
+		return fmt.Errorf("failed to copy fuzzing report: %v", err)
 	}
 
 	// Write the fuzzing_with_seeds report to the final report
 	_, err = io.Copy(finalReport, fuzzingWithSeedsReport)
 	if err != nil {
-		log.Fatalf("Failed to copy fuzzing_with_seeds report: %v", err)
+		return fmt.Errorf("failed to copy fuzzing_with_seeds report: %v", err)
 	}
 
 	fmt.Println("Final report generated: final_report.txt")
+	return nil
 }
